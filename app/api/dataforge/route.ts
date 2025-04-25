a9fe10b5972009a9937c0363c2b6192bcb5ff8c9
deadmanShadow <raihanshamil33@gmail.com>
2025-04-25 23:57:35 +0600

    fixed the next response and adding the server from database appwrite

--- app/api/dataforge/route.ts
+++ app/api/dataforge/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 
 const database = new Databases(client);
 
-// create data
+// create a new docs
 async function createDataForge(data: { term: string; dataforge: string }) {
   try {
     const response = await database.createDocument(
@@ -20,8 +20,8 @@ async function createDataForge(data: { term: string; dataforge: string }) {
   }
 }
 
-// fetch data
-async function fetchDataForge() {
+// list all docs
+async function fetchAllDataForge() {
   try {
     const response = await database.listDocuments(
       process.env.NEXT_PUBLIC_APPWRITE_DBID as string,
@@ -35,29 +35,84 @@ async function fetchDataForge() {
   }
 }
 
+// get a single document by id
+async function fetchDataForgeById(id: string) {
+  try {
+    const document = await database.getDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DBID as string,
+      "dataforge",
+      id
+    );
+    return document;
+  } catch (error) {
+    console.error("Error fetching document by ID:", error);
+    throw new Error();
+  }
+}
+
+// delete a document by id
+async function deleteDataForgeById(id: string) {
+  try {
+    await database.deleteDocument(
+      process.env.NEXT_PUBLIC_APPWRITE_DBID as string,
+      "dataforge",
+      id
+    );
+  } catch (error) {
+    console.error("Error deleting document:", error);
+    throw new Error();
+  }
+}
+
+// post data
 export async function POST(req: Request) {
   try {
     const { term, dataforge } = await req.json();
-    const data = { term, dataforge };
-    const response = await createDataForge(data);
-    return NextResponse.json({
-      message: "Data created successfully",
-    });
+    await createDataForge({ term, dataforge });
+    return NextResponse.json({ message: "Data created successfully" });
+  } catch {
+    return NextResponse.json({ error: "Error creating data" }, { status: 500 });
+  }
+}
+
+// get list all
+export async function GET() {
+  try {
+    const dataforge = await fetchAllDataForge();
+    return NextResponse.json(dataforge);
+  } catch {
+    return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
+  }
+}
+
+// fetch by id
+export async function GET_BY_ID(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const document = await fetchDataForgeById(params.id);
+    return NextResponse.json(document);
   } catch {
     return NextResponse.json(
-      {
-        error: "Error creating data",
-      },
+      { error: "Error fetching document" },
       { status: 500 }
     );
   }
 }
 
-export async function GET() {
+// delete an id
+export async function DELETE_BY_ID(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    const dataforge = await fetchDataForge();
-    return NextResponse.json(dataforge);
+    await deleteDataForgeById(params.id);
+    return NextResponse.json({ message: "Document deleted successfully" });
   } catch {
-    return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Error deleting document" },
+      { status: 500 }
+    );
   }
 }
